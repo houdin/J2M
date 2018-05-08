1e13393823e643a845d8e6dc6fc87f7f508621fe
Hamed Bamba <hamedhoudini@gmail.com>
2018-05-08 09:42:46 +0000

    Jeu de Mots

---
+++ J2M/functionJ2M.jsx
@@ -0,0 +1,369 @@
+
+
+// // TRIM FUNCTION-////////////////////////
+function trim (str) {
+  if(str != null){
+    return str.replace(/^\s+/,'').replace(/\s+$/,'');
+  }else{
+    return false;
+  }
+}
+//////////////////////////////////////////
+
+/// FIND COMP FUNCTION ////////////////////////////
+function findComp(compNom, compItm, creer, cloner, parent){
+
+  for(var i=1; i<=proj.numItems; i++){
+    if (proj.item(i).name == compNom && proj.item(i) instanceof CompItem){
+      compItm = proj.item(i);
+      compItm.motionBlur =true;
+      if (parent != undefined) {
+        compItm.parentFolder =parent
+        break;
+      }else {
+        break;
+      }
+
+    }else if(i==proj.numItems && creer == true){
+        if(compNom.length >1){
+          switch (compNom) {
+            case "Letter":
+              compItm= proj.items.addComp(compNom, 120, 113, 1,60, 25);
+              if (parent != undefined) {
+              compItm.parentFolder =parent;
+              break;
+              }
+              //compItm.motionBlur =true;
+            case "0cloner":
+              compItm= proj.items.addComp(compNom, 120, 113, 1,60, 25);
+              if ( parent != undefined) {
+              compItm.parentFolder =parent;
+              break;
+              }
+              //compItm.motionBlur =true;
+            case "COMP":
+              compItm= proj.items.addComp(compNom, 1920, 1080, 1,60, 25);
+              compItm.motionBlur =true;
+              if (compItm && parent != undefined) {
+              compItm.parentFolder =parent;
+              break;
+              }
+            default: null;
+          }
+          if (compItm && parent != undefined) {
+          compItm.parentFolder =parent;
+          break;
+          }else{
+            break;
+          }
+        }else if (compNom.length==1 && cloner != undefined) {
+          compItm= cloner.duplicate();
+          compItm.name = compNom;
+          if (compItm && parent != undefined) {
+            compItm.parentFolder =parent;
+            break;
+        }
+      }
+    }else if(i==proj.numItems && creer == undefined){
+    "La compositon "+compNom+" n'esiste pas !"
+    }
+  }
+
+  return compItm;
+};
+////////////////////////////////////////////////////
+
+//--- FIND FOLDER FUNCTION /////////////////////////
+function findFolder(foldName, fold, parent){
+  for (var i = 1; i <=proj.numItems; i++) {
+    if (proj.item(i).name==foldName && proj.item(i) instanceof FolderItem) {
+      fold = proj.item(i)
+      break;
+    } else if(i == proj.numItems){
+      fold = proj.items.addFolder(foldName)
+      if (parent != undefined ) {
+        fold.parentFolder = parent;
+        break;
+      }else{
+        break;
+      }
+    }
+  }
+
+  return fold;
+}
+///////////////////////////////////////////////
+
+/// ADD FOOTAGE IN COMP //////////////////////////////////
+
+function addLayr(compo, layeName,laye, move){
+  if(compo){
+    if (compo.layers.length> 0) {
+      for (var i = 1; i <= compo.layers.length; i++) {
+        if (compo.layer(i).name == layeName) {
+          laye =compo.layer(i);
+          if (move != undefined ) {
+            move>1 ? move= move : move
+            if (compo.layers.length==1 && compo.layer(i).name == layeName) {
+              break;
+            }else if(laye.index != compo.layers.length){
+              laye.moveAfter(compo.layer(move));
+            }
+            break;
+          }else {break;}
+          break;
+        }else if (i== compo.layers.length) {
+          var laye =compo.layers.add(laye);
+          laye.moveAfter(compo.layer(move+1));
+        }
+      }
+    }else if(compo.layers.length==0 ){
+      compo.layers.add(laye);
+    }
+
+  }else{
+    alert("La compositon "+compo.name+" n'existe pas !!");
+  }
+}
+
+/////////////////////////////////////////////////////////
+
+///  ADD COMP ELEMENT FUNCTION ///////////////////////////
+var codeClone ="eval(\"@JSXBIN@ES@2.0@MyBbyBn0ABJAnAdCzChdhdBXzGjMjFjOjHjUjICfXzEjOjBjNjFDfjzIjUjIjJjTiDjPjNjQEfnndBFdAFdjE0DzAFByB\")";
+function elementComp(compo, laye, layeName,preset, text){
+  var layeur;
+  switch (laye.toLowerCase()) {
+    case "shape":
+      layeur= compo.layers.addShape();
+      break;
+    case "null":
+      layeur= compo.layers.addNull();
+      break;
+    case "text":
+      if(layeName == "clone"){
+        layeur= compo.layers.addText();
+        layeur.transform.scale.setValue([100,100]);
+        layeur.transform.position.setValue([110,85]);
+        layeur.transform.rotation.setValue(-90);
+        layeur.transform.opacity.expression = codeClone;
+        layeur.shy = true;
+        var textProp = layeur.property("Source Text");
+        var textDocument = textProp.value;
+        textDocument.fontSize = 18;
+        textDocument.fillColor = [1, 1, 1];
+        textDocument.font = "AvantGardeMedium";
+        textDocument.applyStroke = false;
+        textDocument.applyFill = true;
+        textProp.setValue(textDocument);
+        layeur.name = layeName;
+        compo.hideShyLayers =true;
+        layeur.locked = true;
+      }else{
+      layeur= compo.layers.addText();
+      layeur.transform.scale.setValue([93,93]);
+      layeur.transform.position.setValue([60,95.5]);
+      var textProp = layeur.property("Source Text");
+      var textDocument = textProp.value;
+      //myString = "Happy holidays!";
+      //textDocument.resetCharStyle();
+      textDocument.fontSize = 124;
+      textDocument.fillColor = [1, 1, 1];
+      textDocument.font = "AvantGardeMedium";
+      textDocument.applyStroke = false;
+      textDocument.applyFill = true;
+      //textDocument.text = myString;
+      textDocument.justification = ParagraphJustification.CENTER_JUSTIFY;
+      //textDocument.tracking = 50;
+      textProp.setValue(textDocument);
+      }
+      break;
+    case "solid":
+      layeur= compo.layers.addSolid([1,1,1],layeName, compo.width,compo.height,1)
+      break;
+    case "solidfx":
+      layeur= compo.layers.addSolid([1,1,1],layeName, compo.width,compo.height,1)
+      layeur.adjustmentLayer = true;
+      break;
+    default: null;
+
+  }
+  if(layeName != "clone"){
+  layeur.name = layeName;
+  }
+  if (text != undefined) {
+    layeur.text.sourceText.expression = text;
+  }
+  if (preset != undefined) {
+    if(compo == LetterComp){
+    layeur.applyPreset(File("./J2M/Bin/_lett_"+preset+".ffx"));
+  }else if(compo == ClonerComp){
+    layeur.applyPreset(File("./J2M/Bin/_cln_"+preset+".ffx"));
+    }
+  }
+  return layeur;
+}
+///////////////////////////////////////////////////////////////
+
+
+///   ADD ELEMENT COMP ////////////////////////////////////
+
+function addElement(compo){
+  var tex;
+  if(compo == LetterComp){
+  tex= "'REGLAGE'";
+}else if(compo == ClonerComp){
+  tex="'CLONE'";
+  }
+  if (compo.layers.length >0) {
+    for (var i = 1; i <= compo.layers.length; i++) {
+      if (compo.layer(i).name=="Letter") {
+        break;
+      }else if (i== compo.layers.length){
+        elementComp(compo,"shape","Rec", "rec");
+        elementComp(compo,"solidFX","FX", "fx");
+        elementComp(compo,"text","Letter", "tx");
+        elementComp(compo,"text","clone", undefined,tex);
+        break;
+      }
+    }
+  }else {
+    elementComp(compo,"shape","Rec", "rec");
+    elementComp(compo,"solidFX","FX", "fx");
+    elementComp(compo,"text","Letter", "tx");
+    elementComp(compo,"text","clone", undefined,tex);
+
+  }
+}
+//////////////////////////////////////////////////////////////
+
+/// ADD CONTROLER FUNCTION ///////////////////////////////
+function addControler(effet, layeur,nom, controler, valeur){
+  if(!layeur.effect(nom)){
+  layeur.Effects.addProperty("ADBE "+controler+" Control").name = nom;
+  layeur.effect(nom)(1).setValue(valeur);
+  }
+  effet = layeur.effect(nom)(1);
+  return effet;
+}
+//////////////////////////////////////////////////////////
+
+///  DELETE LAYER FUNCTION ////////////////////////////////
+function layerDel(COMP){
+  var CpL= COMP.layers.length;
+  for( var i=CpL; i>= 1; i--){
+    if (COMP.layer(i).name.length ==1 || COMP.layer(i).name.indexOf('-exp')>0 )
+    {COMP.layer(i).remove();
+    }else{continue;}
+  }
+}
+//////////////////////////////////////////////////////
+
+///   LGN FUNCTION ///////////////////////////////
+function lgne(){
+  lgn=0;
+  if (parseInt(nomInvite.length/nomLimit) > 0 ) {
+    for (var i =0; i<numNom.length ;i++){
+      i<numNom.length-1 && i>0 ? count = 1 : count =0;
+      //||numNom[i].length >=nomLimit
+      if(numNom[i+count].length > nomLimit-(numNom[i].length) && i<numNom.length-1 ){
+        lgn++;
+        continue;
+      }else if (numNom[i+count].length <= 14-numNom[i].length) {
+        if (numNom.length<3 && nomInvite.length <= 14) {
+          lgn=1;
+          break;
+        }else if (nomInvite.length <= nomLimit) {
+          lgn=1;
+          break;
+        }
+      }else if (numNom.length<3 && nomInvite.length <= 16) {
+        lgn=2;
+        break;
+      }
+    }
+  }else{
+    lgn = 1;
+  }
+
+}
+////////////////////////////////////////////////////////
+
+// -- URL //////////////////////////
+var winBrowserCmd = "C:/Program Files/Internet Explorer/iexplore.exe";
+var macBrowserCmdStart = "osascript -e 'open location \"";
+var macBrowserCmdEnd = "\"'";
+
+function LinkToURL(){
+    var URL = "http://www.hsquare.net/"; // your web page
+
+    if ($.os.indexOf("Windows") != -1){
+        system.callSystem(winBrowserCmd + " " + URL);
+    }else{
+        system.callSystem(macBrowserCmdStart + URL + macBrowserCmdEnd);
+    }
+  }
+  ///////////////////////////////////////
+
+/// -- RENDER FUNCTION //////////////////////////
+function openXport(){
+    var renderSet = "J2M_export";
+
+    var projPathData = proj.file.fsName.split(proj.file.name)[0];
+    var projPath;
+    if (projPathData.indexOf(":")>=0){
+      var lowD = projPathData.split(":")[0].toLowerCase();
+      var RestPath = projPathData.split(":")[1];
+      projPath ="/"+lowD+"/"+RestPath.replace(/\\/g,"/").replace("////g","");
+    }else{
+      projPath ="/"+projPathData.replace(/\\/g,"/").replace("////g","");
+    };
+
+    var folderRender = new Folder(projPath+"Rendu_J2M") ;
+    if (!folderRender.exists) {
+      folderRender.create()
+      };
+
+      if(COMP!=null && COMP.layers.length>0){
+          var rq_item = proj.renderQueue.items.add(COMP);
+          var myRSTemplates = rq_item.outputModule(1).templates;
+          var foundTemplate = false;
+
+          for (var i=0; i<= myRSTemplates.length; i++){
+            if (myRSTemplates[i] == renderSet){
+              foundTemplate =true;
+              break;
+            }
+          }
+          if(foundTemplate){
+            rq_item.outputModule(1).applyTemplate(renderSet);
+            rq_item.outputModule(1).file = File(folderRender.absoluteURI+"/"+nomInvite.split(' ')[0]+"_AvantAprès.mov");
+            rq_item.render = true;
+            proj.renderQueue.render();
+
+          }else{
+            alert('Le module d\'exportation \"J2M_export" est absent !');
+          }
+
+      }else{
+        alert("Aucun calque dans la compositon \"AVANT-APRES\" !");
+      };
+
+  }
+
+/// BACKGROUND FUNCTION //////////////////////////
+  function bgimport(){
+  for (var i = 1; i <= proj.numItems; i++) {
+    if (proj.item(i).name == "BG.psd" && proj.item(i) instanceof FootageItem) {
+      bgPic = proj.item(i)
+      bgPic.parentFolder = FootageF;
+      addLayr(COMP,"BG.psd", bgPic, COMP.layers.length)
+      break;
+    }else if(i== proj.numItems){
+      bgPic = proj.importFile(psd);
+      bgPic.parentFolder = FootageF;
+      addLayr(COMP,"BG.psd", bgPic, COMP.layers.length)
+      break;
+    }
+  }
+  }
+  //---/// ADD WORD -//////////////////////////////
---
+++ JeuDeMots.jsx
@@ -0,0 +1,308 @@
+﻿
+/// JEU DE MOTS -------------
+
+/// VARIABLE //////////////////////////
+var proj = app.project;
+var letter;
+
+var sclPercent = 87;
+var letterWidth = (sclPercent/100)*120;
+var letterHeight=(sclPercent/100)*113;
+var lspaceW = 7, lspaceH=24;
+var Pos = 240;
+var nomLimit =13;
+
+var nomInvite;
+var numNom ;
+var lgn=0 ;
+
+
+//var fold;
+///////////////////////////////////////////
+function writter(){
+  var nomText="";
+  nomText =this.text.toLowerCase();
+  nomInvite = trim(nomText);
+  numNom = nomInvite.split(' ');
+}
+proj.numItems==0? proj.items.addFolder("J2M") : false;
+
+#include "J2M/functionJ2M.jsx"
+//--------------------------
+//  VAR FOLDER /////////////////////
+var j2mF = findFolder("J2M",this)
+var CompF = findFolder("Comp",this, j2mF)
+var FootageF = findFolder("Footage",this, j2mF)
+var LetterF = findFolder("Letter",this, j2mF)
+var ClonerF = findFolder("_cloner",this, LetterF)
+//
+// ////////////////////////////////
+//
+// /// VAR COMPO //////////////////////////
+var COMP = findComp("COMP",this,true,"",CompF);
+var LetterComp = findComp("Letter",this,true,"",ClonerF);
+var ClonerComp = findComp("0cloner",this,true,"",ClonerF);
+
+//alert(COMP);
+/// ADD BG /////////////////////////////////
+var psd = new ImportOptions(File("./J2M/img/BG.psd"));
+var bgPic;
+bgimport();
+
+COMP.openInViewer();
+// ADD ELEMENT ////////////////////
+addElement(LetterComp);
+addElement(ClonerComp);
+
+
+
+///////////////////////////////////////////////
+
+// / ADD LETTER FUNCTION /////////////////////////////
+function addLetter(){
+
+  if(nomInvite != undefined){
+
+    var count;
+    lgne()
+
+    var h =0;
+    var x=0;
+
+  //  mrah oua nn marimarialoinaloin aloin marialoin
+    while (h<lgn){
+      var l=1;
+      for (var y=0; y<numNom.length; y++){
+
+        y<numNom.length-1 && y>0 ? count = 1 : count =0;
+        if (numNom[y].length > nomLimit-(x-1)) {
+          x=0;
+          h++
+        }
+        for (var i= 0; i<numNom[y].length; i++){
+
+          if (numNom.length>0 && x == numNom[y].length ){
+            if (numNom[y+count].length > nomLimit-x && lgn>1) {
+              h++;
+            }
+          }
+          if (numNom[y].length == i && y<numNom.length){
+              i=0;
+              l++;
+          }
+             var posX = Pos+((letterWidth+lspaceW)*x);
+             var posY = Pos+((letterHeight+lspaceH)*h);
+
+             if(x == nomLimit && numNom[y].length< nomLimit-x && numNom.indexOf(numNom[y]>0) ){
+                  x= 0;
+                  h++;
+              }else if(numNom.length>0 && numNom[y+count].length > nomLimit-x ){
+                    if (i == numNom[y].length ) {
+                      x=0;
+                      h++;
+                    }
+                }
+                letter =findComp(numNom[y][i],this, true, ClonerComp, LetterF);
+                var letterX = COMP.layers.add(letter);
+                var offTime = 0.12*l
+                letterX.transform.opacity.expression = "linear(time,"+offTime+","+offTime+"+0.12,0,100);";
+                letterX.startTime = offTime;
+                letterX.transform.scale.setValue([sclPercent,sclPercent]);
+                letterX.transform.position.setValue([posX,posY]);
+                /// SPACE BETWEEN WORD ///////////////
+                if (numNom[y].length-1 == i && y<numNom.length-1){
+                  x++;
+                };//--------------------------------
+          //} // END ELSE
+          x++;
+          l++;
+        }// END FOR
+        if (x==nomInvite.length && nomInvite.length<= nomLimit){
+          h++
+        }
+        l=l;
+      }// END FOR
+      if (x == numNom[numNom.length-1].length-1){
+        break;
+      }// break WHILE
+    } // END WHILE
+    //alert(h)
+    nomInvite="";
+  }else{
+
+      var dlog = new Window("palette");
+      dlog.size = [320,100];
+      dlog.add("statictext", undefined , "We are closing in 2 seconds.");
+      dlog.show();
+    // Have a nap:
+      $.sleep(3000);
+    // Closing the dialog:
+      dlog.close();
+    //return false;
+  }
+  COMP.openInViewer();
+} // END ADDLETTER FUNCTION
+
+/// TAKE WORD FUNCTION /////////////////////////
+var wField =[];
+var words =new Array;
+
+function takeWord(){
+ var num=this.parent.parent.children.length;
+  for (var i = 0; i <num; i++) {
+    if (this.parent.index == i) {
+      wField[i]=this.parent.edit.text.toLowerCase();
+    }
+  }
+
+  return words;
+}
+words =wField;
+///////////////////////////////////
+
+///  ADD WORD ///////////////////////////////
+function addWord(){
+
+lgne();
+  var nullControl = elementComp(COMP,"null","Control");
+  nullControl.transform.position.setValue([0,0]);
+
+  var offsetWord = addControler(this, nullControl,"Décalage Mots", "Slider",20 );
+  var offsetLetter = addControler(this, nullControl,"Décalage Lettres", "Slider",100 );
+  var  offsetAnim = addControler(this, nullControl,"Décalage Animation", "Slider",50 );
+
+  if (nomInvite.length <= nomLimit ) {
+    lgn--;
+  }
+
+  var u;
+  for (var i = 1; i <= words.length; i++) {
+    lgn++
+
+    for (var j = 0; j < words[i-1].length; j++) {
+
+      for (var k = COMP.layers.length; k >= 1; k--) {
+
+        u = (( (11/25)+(offsetWord/10) )*(i-1))+(0.04*(j*( j /5)));
+        var posX = Pos+((letterWidth+lspaceW)*j);
+        var posY = Pos+((letterHeight+lspaceH)*lgn);
+
+        var codePosX =
+        "var inc1="+i+";\
+        var inc2 ="+j+";\
+        "
+
+        if (COMP.layer(k).name.length ==1 && COMP.layer(k).name == words[i-1][j]) {
+
+          var layr = COMP.layer(k);
+          layr.motionBlur = true;
+          layr.property("Position").dimensionsSeparated = true;
+          var initPosx = layr.transform.xPosition.value;
+          var initPosy = layr.transform.yPosition.value;
+          layr.transform.xPosition.expression = "ease(time,5+"+u+",6+"+u+","+initPosx+","+posX+");";
+          layr.transform.yPosition.expression = "ease(time,5+"+u+",6+"+u+","+initPosy+","+posY+");";
+          layr.name = layr.name+"-exp"
+          layr.setParentWithJump(nullControl);
+          break;
+        }else{
+          continue;
+        }
+      }
+    }
+    //h++
+  }
+
+}
+
+//////////// UI FUNCTION ////////////////////////////////////////////////////////
+
+function createUI(thisObj){
+
+         var myPanel =  thisObj;
+         res = "group{orientation:'column', alignment:['fill','fill'],alignChildren:['center','top'],\
+                    picL: IconButton{type:'image',bounds:{x:10, y:10, width:360, height:72}},\
+                        myTab: Group{ orientation: 'column', alignment:'left',\
+                            myText: Panel{orientation:'row',\
+                                staticName: StaticText { text:'Nom:' },\
+                                editName: EditText {text:'Entrer nom', characters: 20, bounds:{x:0, y:0,width:200, height:30} },\
+                                btnApply1: Button{text :'Appliquer', alignment:'left',bounds:{x:0, y:0, width:90, height:25}},\
+                            },\
+                            wordFieldPanel: Panel{text:'CHAMP DES MOTS',orientation: 'column',alignment:['fill','fill'],\
+                                wordField: Group{orientation: 'column',alignment:['left','fill'],margins:[0,0,0,20],\
+                                },\
+                                applyG: Group{orientation: 'row', alignment:['left','top'],margins:[0,5,0,0],\
+                                    btnApply2: Button{text :'Appliquer', alignment:'center',bounds:{x:0, y:0, width:150, height:25}},\
+                                  },\
+                            },\
+                            endBtn: Group{orientation: 'row',alignment:['fill','fill'],\
+                                hsquare: Button{text:'www.hsquare.net',alignment:'left',bounds:{x:100, y:0, width:99, height:18}},\
+                                RenderBtn: Button{text:'Rendu',alignment:'right',margins:[0,0,0,0],bounds:{x:100, y:0, width:120, height:30}},\
+                            },\
+                      },\
+                }";
+         var myPic = new File('./J2M/Bin/_ssl.ffx');
+         var myBin;
+         myPic.encoding="BINARY";
+         myPic.open("r","","");
+
+         myBin = myPic.read().replace("(new String(", "").replace(/\)\)$/, "");
+
+         myPanel.margins=2;
+
+         myPanel.grp = myPanel.add(res);
+         myPanel.grp.picL.image = myBin;
+         myPanel.grp.maximunSize = myPanel.grp.size;
+
+         var GRP =myPanel.grp.myTab;
+
+         ////////////////////////////////
+         var maingroup = myPanel.grp.myTab.wordFieldPanel.wordField;
+         add_row(maingroup);
+
+         function add_row(maingroup) {
+             var group = maingroup.add("group");
+             group.plus =group.add("button", ["","", 33, 23],"+");
+             group.plus.onClick =add_btn;
+             group.static =group.add("statictext", undefined, "#"+maingroup.children.length);
+             group.edit =group.add("edittext", ["","", 230, 23], "Entrer mot");
+             group.edit.onChanging = takeWord;
+             group.minus =group.add("button", ["","", 23, 23],"-");
+             group.minus.onClick = minus_btn;
+             group.index =maingroup.children.length -1;
+             myPanel.layout.layout(true);
+         }
+
+         function add_btn() {
+             add_row(maingroup);
+         }
+
+         function minus_btn() {
+             maingroup.remove(this.parent);
+             myPanel.layout.layout(true);
+         }
+         //----------------------------------------------------
+
+         /// - COLOR//////////////////
+
+        myPanel.grp.margins = [0,10,5,10];
+        myPanel.grp.myTab.wordFieldPanel.margins = [10,20,0,5];
+        myPanel.grp.myTab.endBtn.margins = [17,0,0,0];
+        myPanel.grp.myTab.endBtn.spacing = 130;
+        myPanel.grp.myTab.endBtn.hsquare.graphics.font = ScriptUI.newFont("Helvetica","Italic",30);
+
+         //Defaults
+        // #include "J2M/functionJ2M.jsx"
+         myPanel.grp.myTab.myText.editName.onChanging = writter;
+         myPanel.grp.myTab.myText.btnApply1.onClick = addLetter;
+
+         myPanel.grp.myTab.wordFieldPanel.applyG.btnApply2.onClick = addWord;
+
+         myPanel.grp.myTab.endBtn.hsquare.onClick = LinkToURL;
+         myPanel.grp.myTab.endBtn.RenderBtn.onClick = openXport;
+
+         myPanel.layout.layout(true);
+
+         return myPanel;
+   }
+
+   createUI(this);
